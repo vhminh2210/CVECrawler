cdb24a63f79b079be4425ca4fdff9e3743253dc2
vhminh2210 <116864211+vhminh2210@users.noreply.github.com>
2024-01-06 13:09:28 +0700

    Add affected products

--- crawl.py
+++ crawl.py
@@ -69,27 +69,70 @@ def parseCommit(url):
     return dict
 
 def parseMetrics(metrics):
-    ks = metrics.keys()
-    ks = sorted(ks, reverse= True)
+    ks = len(metrics)
+    ks = sorted(range(ks), key= lambda x:list(metrics[x].keys())[0])
     for k in ks:
-        if k == 'other':
+        type_str = list(metrics[k].keys())[0]
+        if type_str == 'other':
             continue
         # Pick the latest cvss version
         dict = {
             'scoreType' : k,
-            'baseScore' : metrics[k]['baseScore'],
-            'vectorString' : metrics[k]['vectorString']
+            'baseScore' : metrics[k][type_str]['baseScore'],
+            'vectorString' : metrics[k][type_str]['vectorString']
         }
         return dict
     
     return None
 
+def parseAffected(affected):
+    res = []
+    for product in affected:
+        
+        # Default status
+        defaultStatus = 'unknown'
+        if 'defaultStatus' in product.keys():
+            defaultStatus = product['defaultStatus']
+
+        # Product/Open source name
+        aName = ""
+        if 'packageName' in product.keys():
+            aName = product['package']
+        if 'product' in product.keys():
+            aName = product['product']
+
+        # Vendor/URL
+        source = ""
+        if 'vendor' in product.keys():
+            source = product['vendor']
+        if 'collectionURL' in product.keys():
+            source = product['collectionURL']
+
+        # Version windows, following CVE format
+        pdict = {
+            'defaultStatus' : defaultStatus,
+            'name' : aName,
+            'source' : source,
+        }
+        if 'versions' in product.keys():
+            pdict['versions'] = product['versions']
+        else:
+            pdict['versions'] = None
+
+        # Other attributes
+        added_list = ['defaultStatus', 'packageName', 'product', 'vendor', 'collectionURL', 'versions']
+        for k in product.keys():
+            pdict[k] = product[k]
+
+        res.append(pdict)
+    
+    return res
+
 
 def crawl_container(ctn):
-    # Vulnerable source code & Patched version :
     container = {}
 
-    # Crawling commits
+    # Crawling commits for vulnerable source code, patched version, software versions
     commits = []
     if 'references' in ctn.keys():
         for url_dict in ctn['references']:
@@ -101,17 +144,30 @@ def crawl_container(ctn):
     # Crawling metrics
     if 'metrics' in ctn.keys():
         metric_dict = parseMetrics(ctn['metrics'])
-        if dict != None:
+        if metric_dict != None:
             container['metrics'] = metric_dict
 
-    
+    # Relevent exploit info: Crawling affected products
+    if 'affected' in ctn.keys():
+        affected_dict = parseAffected(ctn['affected'])
+        container['affected'] = affected_dict
+
+    return container
         
 
 def crawl(json_path):
+    formatted_data = {}
     with open(json_path, 'r') as file:
         data = json.load(file)
         cna = data['containers']['cna']
-        crawl_container(cna)
+        formatted_data['cna'] = crawl_container(cna)
+        if 'adp' in data['containers'].keys():
+            adp = data['containers']['adp']
+            formatted_data['adp'] = crawl_container(adp)
+        file.close()
+
+    with open('sample.json', 'w') as file:
+        json.dump(formatted_data, file, indent= 2)
         file.close()
 
 crawl(SAMPLE)
